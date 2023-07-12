bf0cb97c811f6ba76b33279841d5f0d2bb69ea52
Chris Fisher <chris.i.fisher@gmail.com>
2023-07-13 10:59:31 +1200

    Delete unused config files

--- src/authsignal.ts
+++
@@ -1,9 +0,0 @@
-import {Authsignal} from 'react-native-authsignal';
-import {authsignalClientURL, authsignalTenantID} from '../config.mjs';
-
-const authsignalArgs = {
-  tenantID: authsignalTenantID,
-  baseURL: authsignalClientURL,
-};
-
-export const authsignal = new Authsignal(authsignalArgs);
