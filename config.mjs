74be6d049b76ff79fdcfe745bef57e765fea97ac
Chris Fisher <chris.i.fisher@gmail.com>
2023-08-15 14:44:34 +1200

    Update example config file to add dummy values

--- config.mjs
+++ config.mjs
@@ -1,8 +1,9 @@
-export const authsignalTenantID = '921212c3-07e6-4666-a9fe-371cdeb106bc';
+export const authsignalTenantID = 'YOUR_TENANT_ID';
 
-export const authsignalClientURL = 'https://dev-challenge.authsignal.com/v1';
+// Update for your region
+export const authsignalClientURL = 'https://challenge.authsignal.com/v1';
 
-export const authsignalSecretKey =
-  'f/ybpKG7OuXQQ7m5pu6Oo036mOIcj5XMFW3HDyGVuaI3ROxWVJd3hQ==';
+export const authsignalSecretKey = 'YOUR_SECRET_KEY';
 
-export const authsignalServerURL = 'https://dev-signal.authsignal.com/v1';
+// Update for your region
+export const authsignalServerURL = 'https://signal.authsignal.com/v1';
